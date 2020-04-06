6cd6e4d9f6c2df4f7b6e1b12b3fae90d3810a0d6
Nate Franzen <nfranzen@gmail.com>
2020-04-05 20:49:40 -0700

    initial

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/ntfrnzn/kube-put
+
+go 1.14
+
+require (
+	github.com/imdario/mergo v0.3.9 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	k8s.io/apimachinery v0.18.0
+	k8s.io/client-go v0.18.0
+	k8s.io/utils v0.0.0-20200327001022-6496210b90e8 // indirect
+	sigs.k8s.io/kind v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	// "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	//utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// Scheme is the basic k8s scheme
+var Scheme *runtime.Scheme
+
+func init() {
+	Scheme = scheme.Scheme //runtime.NewScheme()
+}
+
+func main() {
+
+	var kubeconfig string
+	var file string
+
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+	flag.StringVar(&file, "file", "", "json or yaml file describing kubernetes object")
+	flag.Parse()
+
+	if kubeconfig == "" {
+		kubeconfig = os.Getenv("KUBECONFIG")
+	}
+
+	if file == "" {
+		log.Fatal("must supply an input file")
+	}
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	obj, err := loadObject(file)
+	if err != nil {
+		panic(err.Error())
+	}
+	if obj == nil {
+		log.Fatal("oops, no object")
+	}
+
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	fmt.Printf("%s\n", gvk.GroupKind())
+
+	// create the dynamic client from kubeconfig
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// convert the runtime.Object to unstructured.Unstructured
+	unstructuredData, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		panic(err.Error())
+	}
+	unstructuredObj := &unstructured.Unstructured{
+		Object: unstructuredData,
+	}
+	// create the object using the dynamic client
+	resource := schema.GroupVersionResource{Version: gvk.Version, Resource: kindToResource(gvk.Kind)}
+
+	createdUnstructuredObj, err := dynamicClient.Resource(resource).Namespace("default").Create(context.TODO(), unstructuredObj, metav1.CreateOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println(createdUnstructuredObj.GetName())
+
+}
+
+func loadObject(filename string) (runtime.Object, error) {
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	// fmt.Println(string(data))
+
+	decoder := serializer.NewCodecFactory(Scheme, serializer.EnableStrict).UniversalDeserializer()
+	obj, _ /* gvk */, err := decoder.Decode(data, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	// fmt.Println("obj: ", obj)
+	// fmt.Println("gvk: ", gvk)
+
+	return obj, nil
+}
+
+func kindToResource(kind string) string {
+	return strings.ToLower(kind) + "s"
+}
